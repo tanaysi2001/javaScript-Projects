49b74a4795eb358e7db6a55cbcdb6e206277067d
tanaysi2001 <thanaysi39@gmail.com>
2026-02-07 22:55:49 +0530

    delete functionality added

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -1,1 +1,76 @@
-st
+let tasks = [];
+
+
+//function to create delete task
+const deleteTask=(index)=>{
+tasks.splice(index,1)//telling that at the current given index remove one item
+updateTaskList();
+}
+
+//function for editing the task
+
+const editTask=(index)=>{
+let taskInput=document.getElementById("taskInput");
+taskInput.value=tasks[index].text;//here the value inside the object will be passed to input field value
+// tasks[index].text=taskInput.value;
+//when we click on the task value is updated in the input field and we have to delete the task
+
+tasks.splice(index,1);
+updateTaskList();
+
+
+}
+
+//function for toggling task
+
+const toggleTaskComplete=(index)=>{
+tasks[index].completed=!tasks[index].completed;
+updateTaskList();
+// console.log(tasks)
+}
+
+//function for updating the task
+const updateTaskList = () => {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `<div class="taskItem" >
+                            <div class="task ${task.completed ? "completed" : ''}" >
+                                <input type="checkbox" class="checkbox" ${task.completed?'checked':''}>
+                                <p>${task.text}</p>
+                            </div>
+
+                            <div class="icons">
+                                <img src="images/edit.png" alt="edit" onclick="editTask(${index})"  class="invert">
+                                <img src="images/bin.png" alt="delete" onclick="deleteTask(${index}) " class="invert">
+                            </div>
+                        </div>`;
+
+    listItem.addEventListener("change", () => toggleTaskComplete(index))
+    taskList.append(listItem);
+  });
+};
+
+//adding the task
+const addTask = () => {
+  const taskInput = document.getElementById("taskInput");
+  const text = taskInput.value.trim();
+
+  if (text) {
+    tasks.push({ text: text, completed: false });
+    console.log(tasks);
+    taskInput.value = "";
+
+    updateTaskList();
+  }
+};
+
+
+//starting of the function
+document.getElementById("newTask").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  addTask();
+});
