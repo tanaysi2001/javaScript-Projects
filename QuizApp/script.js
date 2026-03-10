e401c9fdab6fb682ab7f5d9caea8295e39dcb9f8
tanaysi2001 <thanaysi39@gmail.com>
2026-03-10 12:12:57 +0530

    completed

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -1,3 +1,4 @@
+
 const questions = [
   {
     question: "Which is the largest Animal in the world? ",
@@ -50,6 +51,87 @@ let score = 0;
 function StartQuiz() {
   currentQuestionIndex = 0;
   score = 0;
-  nextButton.innerHTML="Next";//until here when ever we call the this fuction everything will be reset to 0
+  nextButton.innerHTML = "Next"; //until here when ever we call the this fuction everything will be reset to 0
   showQuestion();
+  // console.log(answerButton.children)
 }
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNumber = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNumber + "." + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+
+    //adding event listener for clicking button
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButton.firstChild) {
+    answerButton.removeChild(answerButton.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  //here the event is directly passed from the browser from the add event listener
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+
+  Array.from(answerButton.children).forEach(button=>{
+    if(button.dataset.correct==='true'){
+      button.classList.add("correct");
+    }
+    button.disabled=true;
+  });
+  nextButton.style.display='block';
+
+}
+
+//function to score
+
+function showScore(){
+  resetState();
+  questionElement.innerHTML=`You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML="Play Again"
+  nextButton.style.display='block';
+}
+
+//function for handling the next button
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex<questions.length){
+    showQuestion();
+  }else{
+    showScore();
+  }
+}
+
+//function for nextbutton
+
+nextButton.addEventListener("click",()=>{
+  if(currentQuestionIndex<questions.length){
+    handleNextButton();
+  }else{
+    StartQuiz();
+  }
+})
+StartQuiz();
